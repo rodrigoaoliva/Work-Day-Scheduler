75522a3b4bcdac8d514bc20bccab36b80d37a0c0
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-11-06 16:01:44 -0500

    added functions to load saved events from local storage and added event listener to save events

--- Assets/script.js
+++ Assets/script.js
@@ -20,22 +20,22 @@ $(function () {
 
   updateTimeBlockColors();
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Function to load saved events from local storage
+    function loadEvents() {
+      $(".time-block").each(function () {
+        const eventId = $(this).attr("id");
+        const eventDescription = localStorage.getItem(eventId);
+        $(this).find("textarea").val(eventDescription);
+      });
+    }
+  
+    loadEvents();
+
+  // Add a click event listener to save events to local storage
+  $(".saveBtn").on("click", function () {
+    const eventId = $(this).closest(".time-block").attr("id");
+    const eventDescription = $(this).siblings(".description").val();
+    localStorage.setItem(eventId, eventDescription);
+  });
 });
+
